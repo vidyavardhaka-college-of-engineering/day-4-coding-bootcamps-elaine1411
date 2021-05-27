43dcf71c605944f64679a7167e2f6c2a9054d6a7
Elaine14 <>
2021-05-27 13:49:43 +0000

    print a matrix

--- Program_3.c
+++ Program_3.c
@@ -9,6 +9,33 @@
 #include<stdio.h> 
 int main()
 {
+  int disp[100][200],i,j,m,n;
+
+  printf("Enter the no. of rows:");
+  scanf("%d",&m);
+
+  printf("Enter the no. of columns:");
+  scanf("%d",&n);
+ 
+ for(i=1;i<m+1;i++)
+ {
+   for(j=1;j<n+1;j++)
+   {
+     printf("Enter the elements of disp[%d][%d]:",m,n);
+     scanf("%d",&disp[i][j]);
+   }
+ }
+
+  printf("The matrix is:\n");
+  for(i=1;i<m+1;i++)
+  {
+    for(j=1;j<n+1;j++)
+    {
+      printf("%d ",disp[i][j]);
+    }
+    printf("\n");
+  }
+
   //Declare all the variables[row, col, array a(2D array), i] used in the code
    //Read the Value of row and coloumn of the matrix (i.e size of matrix) using scanf statement
    //Read the elements of matrix a using nested for loop and scanf statment
