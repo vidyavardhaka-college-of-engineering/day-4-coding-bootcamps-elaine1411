5514c9641eedd2884f3b16faa87218bc31fd7111
Elaine14 <>
2021-05-27 17:36:15 +0000

    sum of elements in an array

--- Program_4.c
+++ Program_4.c
@@ -7,6 +7,24 @@
 #include <stdio.h>
 int main()
 {
+  int a[100],i,N,sum=0;
+
+  printf("Enter the number of elements in the array:");
+  scanf("%d",&N);
+
+  printf("Enter the %d elements of the array\n",N);
+  for(i=1;i<N+1;i++)
+  {
+    printf("Enter the %d element of the array:",i);
+    scanf("%d",&a[i]);
+  }
+
+  for(i=1;i<N+1;i++)
+  {
+    sum+=a[i];
+  }
+
+  printf("The sum of all the elements in the array is:%d",sum);
   //Declare all the variables[N, array a(1D array), i, sum=0] used in the code
   //Read the Value of array size N using scanf statement
   //Read the array elements using for loop and scanf statment
